7bca30102dead7f5dceee3008ad1b2135145eef6
Dzmitry Hil <dzmitry.hil@@connectwise.com>
2021-06-21 13:36:33 +0300

    single node runner: run.sh script refactoring (common variables, miner ETHGenesis generation)

--- deploy/single-node-runner/scripts/run.sh
+++ deploy/single-node-runner/scripts/run.sh
@@ -5,68 +5,78 @@ echo "bootstrapping environment"
 
 # Constants
 CURRENT_WORKING_DIR=$(pwd)
+# Name of the network to bootstrap
 CHAINID="testchain"
-CHAINDIR="$CURRENT_WORKING_DIR/testdata"
-gravity=gravity
-#FED=oracle-feeder
-home_dir="$CHAINDIR/$CHAINID"
-
-n0name="gravity0"
-# Folders for nodes
-n0dir="$home_dir/$n0name"
+# Name of the gravity artifact
+GRAVITY=gravity
+# The name of the gravity node
+GRAVITY_NODE_NAME="gravity"
+# The address to run gravity note
+GRAVITY_HOST="0.0.0.0"
+# Home folder for gravity config
+GRAVITY_HOME="$CURRENT_WORKING_DIR/$CHAINID/$GRAVITY_NODE_NAME"
 # Home flag for folder
-home0="--home $n0dir"
-# Config directories for nodes
-n0cfgDir="$n0dir/config"
-# Config files for nodes
-n0cfg="$n0cfgDir/config.toml"
-# App config files for nodes
-n0appCfg="$n0cfgDir/app.toml"
+GRAVITY_HOME_FLAG="--home $GRAVITY_HOME"
+# Config directories for gravity node
+GRAVITY_HOME_CONFIG="$GRAVITY_HOME/config"
+# Config files for gravity node
+GRAVITY_NODE_CONFIG="$GRAVITY_HOME_CONFIG/config.toml"
+# App config files for gravity node
+GRAVITY_APP_CONFIG="$GRAVITY_HOME_CONFIG/app.toml"
 # Common flags
-kbt="--keyring-backend test"
-cid="--chain-id $CHAINID"
+GRAVITY_KEYRING_FLAG="--keyring-backend test"
+GRAVITY_CHAINID_FLAG="--chain-id $CHAINID"
+# Prefix of cosmos addresses
+GRAVITY_ADDRESS_PREFIX=cosmos
+# gravity chain demons
+STAKE_DENOM="stake"
+NORMAL_DENOM="samoleans"
 
-echo "Creating $gravity validator with chain-id=$CHAINID..."
+# This key is the private key for the public key defined in ETHGenesis.json
+# where the full node / miner sends its rewards. Therefore it's always going
+# to have a lot of ETH to pay for things like contract deployments
+ETH_MINER_PRIVATE_KEY="0xb1bab011e03a9862664706fc3bbaa1b16651528e5f0e7fbfcbfdd8be302a13e7"
+ETH_MINER_PUBLIC_KEY="0xBf660843528035a5A4921534E156a27e64B231fE"
+# The host of ethereum node
+ETH_HOST="0.0.0.0"
+
+# ------------------ Init & run gravity ------------------
+
+echo "Creating $GRAVITY_NODE_NAME validator with chain-id=$CHAINID..."
 echo "Initializing genesis files"
 
 # Build genesis file incl account for passed address
-coins="100000000000stake,100000000000samoleans"
+GRAVITY_GENESIS_COINS="100000000000$STAKE_DENOM,100000000000$NORMAL_DENOM"
 
 # Initialize the home directory and add some keys
-$gravity $home0 $cid init n0
-$gravity $home0 keys add val $kbt --output json | jq . >> $n0dir/validator_key.json
+$GRAVITY $GRAVITY_HOME_FLAG $GRAVITY_CHAINID_FLAG init $GRAVITY_NODE_NAME
+$GRAVITY $GRAVITY_HOME_FLAG keys add val $GRAVITY_KEYRING_FLAG --output json | jq . >> $GRAVITY_HOME/validator_key.json
 
 echo "Adding validator addresses to genesis files"
-$gravity $home0 add-genesis-account $($gravity $home0 keys show val -a $kbt) $coins
+$GRAVITY $GRAVITY_HOME_FLAG add-genesis-account "$($GRAVITY $GRAVITY_HOME_FLAG keys show val -a $GRAVITY_KEYRING_FLAG)" $GRAVITY_GENESIS_COINS
 
 echo "Generating orchestrator keys"
-$gravity $home0 keys add --dry-run=true --output=json orch | jq . >> $n0dir/orchestrator_key.json
+$GRAVITY $GRAVITY_HOME_FLAG keys add --dry-run=true --output=json orch | jq . >> $GRAVITY_HOME/orchestrator_key.json
 
 echo "Adding orchestrator keys to genesis"
-n0orchKey="$(jq .address $n0dir/orchestrator_key.json)"
+GRAVITY_ORCHESTRATOR_KEY="$(jq .address $GRAVITY_HOME/orchestrator_key.json)"
 
-jq ".app_state.auth.accounts += [{\"@type\": \"/cosmos.auth.v1beta1.BaseAccount\",\"address\": $n0orchKey,\"pub_key\": null,\"account_number\": \"0\",\"sequence\": \"0\"}]" $n0cfgDir/genesis.json | sponge $n0cfgDir/genesis.json
-jq ".app_state.bank.balances += [{\"address\": $n0orchKey,\"coins\": [{\"denom\": \"samoleans\",\"amount\": \"100000000000\"},{\"denom\": \"stake\",\"amount\": \"100000000000\"}]}]" $n0cfgDir/genesis.json | sponge $n0cfgDir/genesis.json
+jq ".app_state.auth.accounts += [{\"@type\": \"/cosmos.auth.v1beta1.BaseAccount\",\"address\": $GRAVITY_ORCHESTRATOR_KEY,\"pub_key\": null,\"account_number\": \"0\",\"sequence\": \"0\"}]" $GRAVITY_HOME_CONFIG/genesis.json | sponge $GRAVITY_HOME_CONFIG/genesis.json
+jq ".app_state.bank.balances += [{\"address\": $GRAVITY_ORCHESTRATOR_KEY,\"coins\": [{\"denom\": \"$NORMAL_DENOM\",\"amount\": \"100000000000\"},{\"denom\": \"$STAKE_DENOM\",\"amount\": \"100000000000\"}]}]" $GRAVITY_HOME_CONFIG/genesis.json | sponge $GRAVITY_HOME_CONFIG/genesis.json
 
 echo "Generating ethereum keys"
-$gravity $home0 eth_keys add --output=json --dry-run=true | jq . >> $n0dir/eth_key.json
-
-echo "Copying ethereum genesis file"
-cp assets/ETHGenesis.json $home_dir
-
-echo "Adding initial ethereum value"
-jq ".alloc |= . + {$(jq .address $n0dir/eth_key.json) : {\"balance\": \"0x1337000000000000000000\"}}" $home_dir/ETHGenesis.json | sponge $home_dir/ETHGenesis.json
+$GRAVITY $GRAVITY_HOME_FLAG eth_keys add --output=json --dry-run=true | jq . >> $GRAVITY_HOME/eth_key.json
 
 echo "Creating gentxs"
-$gravity $home0 gentx --ip $n0name val 100000000000stake $(jq -r .address $n0dir/eth_key.json) $(jq -r .address $n0dir/orchestrator_key.json) $kbt $cid
+$GRAVITY $GRAVITY_HOME_FLAG gentx --ip $GRAVITY_HOST val 100000000000$STAKE_DENOM "$(jq -r .address $GRAVITY_HOME/eth_key.json)" "$(jq -r .address $GRAVITY_HOME/orchestrator_key.json)" $GRAVITY_KEYRING_FLAG $GRAVITY_CHAINID_FLAG
 
-echo "Collecting gentxs in $n0name"
-$gravity $home0 collect-gentxs
+echo "Collecting gentxs in $GRAVITY_NODE_NAME"
+$GRAVITY $GRAVITY_HOME_FLAG collect-gentxs
 
-echo "Exposing ports and APIs of the $n0name"
+echo "Exposing ports and APIs of the $GRAVITY_NODE_NAME"
 # Switch sed command in the case of linux
 fsed() {
-  if [ `uname` = 'Linux' ]; then
+  if [ ""`uname` = 'Linux' ]; then
     sed -i "$@"
   else
     sed -i '' "$@"
@@ -74,16 +84,23 @@ fsed() {
 }
 
 # Change ports on n0 val
-fsed "s#\"tcp://127.0.0.1:26656\"#\"tcp://0.0.0.0:26656\"#g" $n0cfg
-fsed "s#\"tcp://127.0.0.1:26657\"#\"tcp://0.0.0.0:26657\"#g" $n0cfg
-fsed 's#addr_book_strict = true#addr_book_strict = false#g' $n0cfg
-fsed 's#external_address = ""#external_address = "tcp://'$n0name:26656'"#g' $n0cfg
-fsed 's#enable = false#enable = true#g' $n0appCfg
-fsed 's#swagger = false#swagger = true#g' $n0appCfg
+fsed "s#\"tcp://127.0.0.1:26656\"#\"tcp://$GRAVITY_HOST:26656\"#g" $GRAVITY_NODE_CONFIG
+fsed "s#\"tcp://127.0.0.1:26657\"#\"tcp://$GRAVITY_HOST:26657\"#g" $GRAVITY_NODE_CONFIG
+fsed 's#addr_book_strict = true#addr_book_strict = false#g' $GRAVITY_NODE_CONFIG
+fsed 's#external_address = ""#external_address = "tcp://'$GRAVITY_HOST:26656'"#g' $GRAVITY_NODE_CONFIG
+fsed 's#enable = false#enable = true#g' $GRAVITY_APP_CONFIG
+fsed 's#swagger = false#swagger = true#g' $GRAVITY_APP_CONFIG
 
-$gravity $home0 start --pruning=nothing > $CURRENT_WORKING_DIR/gravity.$n0name.log &>/dev/null
+echo "Starting $GRAVITY_NODE_NAME"
+$GRAVITY $GRAVITY_HOME_FLAG start --pruning=nothing &>/dev/null
 
-#-------------------- Ethereum --------------------
+#-------------------- Run ethereum (geth) --------------------
+
+echo "Adding initial ethereum value for miner"
+jq ".alloc |= . + {\"$ETH_MINER_PUBLIC_KEY\" : {\"balance\": \"0x1337000000000000000000\"}}" assets/ETHGenesis.json | sponge assets/ETHGenesis.json
+
+echo "Adding initial ethereum value for gravity validator"
+jq ".alloc |= . + {$(jq .address $GRAVITY_HOME/eth_key.json) : {\"balance\": \"0x1337000000000000000000\"}}" assets/ETHGenesis.json | sponge assets/ETHGenesis.json
 
 geth --identity "GravityTestnet" \
     --nodiscover \
@@ -94,16 +111,16 @@ geth --identity "GravityTestnet" --nodiscover \
                                --mine \
                                --http \
                                --http.port "8545" \
-                               --http.addr "0.0.0.0" \
+                               --http.addr "$ETH_HOST" \
                                --http.corsdomain "*" \
                                --http.vhosts "*" \
                                --miner.threads=1 \
                                --nousb \
                                --verbosity=5 \
-                               --miner.etherbase=0xBf660843528035a5A4921534E156a27e64B231fE \
-                               > $CURRENT_WORKING_DIR/ethereum.$n0name.log &>/dev/null
+                               --miner.etherbase="$ETH_MINER_PUBLIC_KEY" \
+                               &>/dev/null
 
-#-------------------- Ethereum / Applying contracts --------------------
+#-------------------- Apply ethereum contract --------------------
 
 echo "Waiting for nodes"
 sleep 10
@@ -113,15 +130,15 @@ echo "Applying contracts"
 GRAVITY_DIR=/go/src/github.com/onomyprotocol/gravity-bridge/
 cd $GRAVITY_DIR/solidity
 
-contractAddress=$(npx ts-node \
+CONTRACT_ADDRESS=$(npx ts-node \
                       contract-deployer.ts \
-                      --cosmos-node="http://$n0name:26657" \
-                      --eth-node="http://0.0.0.0:8545" \
-                      --eth-privkey="0xb1bab011e03a9862664706fc3bbaa1b16651528e5f0e7fbfcbfdd8be302a13e7" \
+                      --cosmos-node="http://$GRAVITY_HOST:26657" \
+                      --eth-node="http://$ETH_HOST:8545" \
+                      --eth-privkey="$ETH_MINER_PRIVATE_KEY" \
                       --contract=artifacts/contracts/Gravity.sol/Gravity.json \
                       --test-mode=true | grep "Gravity deployed at Address" | grep -Eow '0x[0-9a-fA-F]{40}')
 
-echo "Contract address: $contractAddress"
+echo "Contract address: $CONTRACT_ADDRESS"
 
 # return back to home
 cd $CURRENT_WORKING_DIR
@@ -129,34 +146,25 @@ cd $CURRENT_WORKING_DIR
 #-------------------- ORCHESTRATOR --------------------
 
 echo "Gathering keys for orchestrators"
-VALIDATOR=$n0name
-COSMOS_GRPC="http://$n0name:9090/"
-COSMOS_RPC="http://$n0name:1317"
-COSMOS_KEY=$(jq .priv_key.value $n0cfgDir/priv_validator_key.json)
-COSMOS_PHRASE=$(jq .mnemonic $n0dir/orchestrator_key.json)
-DENOM=stake
-ETH_RPC=http://0.0.0.0:8545
-ETH_PRIVATE_KEY=$(jq .private_key $n0dir/eth_key.json)
-CONTRACT_ADDR=$contractAddress
+COSMOS_GRPC="http://$GRAVITY_HOST:9090/"
+COSMOS_PHRASE=$(jq .mnemonic $GRAVITY_HOME/orchestrator_key.json)
+ETH_RPC=http://$ETH_HOST:8545
+ETH_PRIVATE_KEY=$(jq .private_key $GRAVITY_HOME/eth_key.json)
 
-rpc="http://0.0.0.0:1317"
-grpc="http://0.0.0.0:9090"
-ethrpc="http://0.0.0.0:8545"
+echo orchestrator --cosmos-phrase="$COSMOS_PHRASE" \
+             --ethereum-key="$ETH_PRIVATE_KEY" \
+             --cosmos-grpc="$COSMOS_GRPC" \
+             --ethereum-rpc="$ETH_RPC" \
+             --fees="$STAKE_DENOM" \
+             --contract-address="$CONTRACT_ADDRESS"\
+             --address-prefix="$GRAVITY_ADDRESS_PREFIX"
 
-echo orchestrator --cosmos-phrase="${COSMOS_PHRASE}" \
-             --ethereum-key="${ETH_PRIVATE_KEY}" \
-             --cosmos-grpc="$grpc" \
-             --ethereum-rpc="$ethrpc" \
-             --fees="${DENOM}" \
-             --contract-address="${CONTRACT_ADDR}"\
-             --address-prefix=cosmos
-
-orchestrator --cosmos-phrase="${COSMOS_PHRASE}" \
-             --ethereum-key="${ETH_PRIVATE_KEY}" \
-             --cosmos-grpc="$grpc" \
-             --ethereum-rpc="$ethrpc" \
-             --fees="${DENOM}" \
-             --contract-address="${CONTRACT_ADDR}"\
-             --address-prefix=cosmos
+orchestrator --cosmos-phrase="$COSMOS_PHRASE" \
+             --ethereum-key="$ETH_PRIVATE_KEY" \
+             --cosmos-grpc="$COSMOS_GRPC" \
+             --ethereum-rpc="$ETH_RPC" \
+             --fees="$STAKE_DENOM" \
+             --contract-address="$CONTRACT_ADDRESS"\
+             --address-prefix="$GRAVITY_ADDRESS_PREFIX"
 
 echo "done"
